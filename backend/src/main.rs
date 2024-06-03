337c705db60e381887e6a93115dd4a03ee047f35
Optics-Boss <->
2024-06-03 20:34:46 +0200

    Started working on the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,24 @@
 #![deny(warnings)]
+use serde::{Deserialize, Serialize};
 use warp::Filter;
+use std::convert::Infallible;
+
+#[derive(Deserialize, Serialize)]
+pub struct Quiz {
+    id: u64,
+    title: String,
+}
 
 #[tokio::main]
 async fn main() {
     // Match any request and return hello world!
     let welcome = warp::path::end().map(|| "Welcome to Hippo Quiz Master");
 
-    let quizes = warp::path("quizes").map(|| "Here are quizes");
+    let quizes = warp::path!("quizes")
+        .and(warp::get())
+        .and_then(list_quizes);
+
+
     let quiz = warp::path("quiz")
         .and(warp::path::param())
         .map(|quiz: String| format!("Quiz number:{}!", quiz));
@@ -19,4 +31,18 @@ async fn main() {
     );
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+pub async fn list_quizes() -> Result<impl warp::Reply, Infallible> {
+    let quiz = Quiz {
+        id: 1,
+        title: "Test".to_string()
+    };
+
+    let quiz2 = Quiz {
+        id: 2,
+        title: "This is Quiz 2".to_string()
+    };
+
+   Ok(warp::reply::json(&[quiz, quiz2]))
 }
