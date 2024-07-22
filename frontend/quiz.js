30ec87dcfd621dcd6239b515ee7d592c13b84e7f
Optics-Boss <->
2024-07-22 22:32:59 +0200

    Finished the quiz with images

--- frontend/quiz.js
+++ frontend/quiz.js
@@ -41,7 +41,7 @@ function getQuestions(quiz) {
 
         this.appendElement("questions_list", 
           `<h2 id="statement">${this.questions[0][i].statement}</h2>
-            <img src="${this.questions[0][i].image}" alt="${this.questions[0][i].statement}">
+            <img class="normal__image" src="http://localhost:3030/static/${this.questions[0][i].image}" alt="${this.questions[0][i].statement}">
             <ul class="normal__list">
               ${shuffledQuestionsArray[0]}
               ${shuffledQuestionsArray[1]}
